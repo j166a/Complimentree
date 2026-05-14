1193cf0ddde24c433e227e983684954b52de8a24
Konkz7 <amara03@live.co.uk>
2026-05-14 14:20:51 +0100

    create static getCompliments function

--- Complimentree/DataModels/Compliment.cs
+++ Complimentree/DataModels/Compliment.cs
@@ -4,5 +4,26 @@
     {
         public int Id { get; set; }
         public string Text { get; set; }
+
+        public static string GetCompliment()
+        {
+            Random rand = new Random();
+            string[] compliments = {"You may not be evergreen, but you are to me.",
+                "I bet squirrels consider you prime real estate.",
+                "You're so down to earth.",
+                "You're very grounded.",
+                "Your leaves are soothing.",
+                "You sway nicely in the wind.",
+                "You're blooming brilliant.",
+                "You're really good at standing still and doing nothing.",
+                "Let's get to the root of your beauty.",
+                "I will never leaf you.",
+                "You've really branched out lately.",
+                "You provide the perfect amount of shade on a hot day.",
+                "I'm really pine-ing for you." };
+
+
+            return compliments[rand.Next(compliments.Length)];
+        }
     }
 }
