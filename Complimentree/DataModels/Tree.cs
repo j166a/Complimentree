09ff769a01e0dd081e5208e70bfe2b8bfd2e3c18
Konkz7 <amara03@live.co.uk>
2026-05-14 15:44:12 +0100

    Add custom compliment functionality

--- Complimentree/DataModels/Tree.cs
+++ Complimentree/DataModels/Tree.cs
@@ -7,6 +7,8 @@
         public (int, int) Location { get; set; }
         public string Size { get; set; }
         public string Species { get; set; }
+
+        public string InputBox { get; set; }
         public List<Compliment> Compliments { get; set; } = new();
 
         public Tree (int id, string name , (int,int) location, string size, string species  )
