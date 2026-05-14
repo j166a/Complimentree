3c7bebd09074c8784a3c1756d6f9c9391ee5e136
Imad Ahmed <j166a775433@gmail.com>
2026-05-14 12:23:44 +0100

    Add data models

---
+++ Complimentree/DataModels/Compliment.cs
@@ -0,0 +1,8 @@
+﻿namespace Complimentree.DataModels
+{
+    public class Compliment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Complimentree/DataModels/Tree.cs
@@ -0,0 +1,13 @@
+﻿namespace Complimentree.DataModels
+{
+    public class Tree
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public (int, int) Location { get; set; }
+        public string Size { get; set; }
+        public string Species { get; set; }
+        public List<Compliment>  Compliments  { get; set; }
+
+    }
+}
